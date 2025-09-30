d761462a4b7bdb295caacdb24a07aa22d686ba03
thedinesh04 <naikd0153@gmail.com>
2025-09-30 20:54:57 +0530

    Frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <h1 className="bg-blue-500 text-black text-3xl text-center p-6">
+                Hello from react + tailwind
+            </h1>
+        </>
+    )
+}
+
+export default App
